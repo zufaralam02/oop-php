b037b6a24bdece02681fc8fb6249cb1d5228cc54
Rofiif Zufar 'Allam <zufaralam02@gmail.com>
2020-07-29 21:57:27 +0700

    add autoloading

---
+++ autoloading/App/Produk/Komik.php
@@ -0,0 +1,27 @@
+<?php
+
+class Komik extends Produk implements InfoProduk
+{
+    public $jumlahHalaman;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jumlahHalaman = 0)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
+        $this->jumlahHalaman = $jumlahHalaman;
+    }
+
+    public function getInfo()
+    {
+        $str = "{$this->judul} | {$this->getLable()} (Rp. {$this->harga})";
+
+        return $str;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Komik : " . $this->getInfo() . " - {$this->jumlahHalaman} halaman.";
+
+        return $str;
+    }
+}
